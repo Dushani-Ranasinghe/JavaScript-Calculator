3f0c163983723ecb34b868479844935f29fddb1a
Dushani Ranasinghe <uththaradushani@gmail.com>
2021-03-09 08:23:18 +0530

    Added comments to html,css and js files

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
- //function that display value 
- function dis(val) 
+  //function to clear the display 
+  function clear() 
+  { 
+      document.getElementById("result").value = "" 
+  } 
+//function to display value 
+ function display(val) 
  { 
      document.getElementById("result").value+=val 
  } 
    
- //function that evaluates the digit and return result 
- function solve() 
+ //function to get the value and return the result 
+ function calculate() 
  { 
      let x = document.getElementById("result").value 
      let y = eval(x) 
      document.getElementById("result").value = y 
  } 
    
- //function that clear the display 
- function clr() 
- { 
-     document.getElementById("result").value = "" 
- } 
